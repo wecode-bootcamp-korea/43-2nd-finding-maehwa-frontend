6d1a9d1c09ef60ce102b233b644dcabb0fe08858
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-03-28 03:06:53 +0900

    :sparkles: Map 레이아웃 및 지도 구현
    :sparkles: KakaoLogin 레이아웃 구현
    
    :bug: KakaoLogin 레이아웃 수정사항 미반영 해결

--- src/pages/KakaoLogin/KakaoLogin.js
+++ src/pages/KakaoLogin/KakaoLogin.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import * as S from './KakaoLogin.style';
 
 const KakaoLogin = () => {
-  return <div>KakaoLogin</div>;
+  return (
+    <S.LoginContainer>
+      <S.SplashImg src="/images/splashImg.png" alt="splashImg" />
+      <Link to="/map">
+        <S.KakaoLoginBtn
+          src="/images/kakao_login_medium_wide.png"
+          alt="kakaoLoginBtn"
+        />
+      </Link>
+      <Link to="/map">
+        <S.LookAroundBtn>로그인 없이 둘러보기</S.LookAroundBtn>
+      </Link>
+    </S.LoginContainer>
+  );
 };
 
 export default KakaoLogin;
