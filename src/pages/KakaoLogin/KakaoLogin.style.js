a3335934cdf767d221629d17eea96f84987d2731
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-03-28 03:06:53 +0900

    :sparkles: Map 레이아웃 및 지도 구현
    :sparkles: KakaoLogin 레이아웃 구현
    
    :bug: KakaoLogin 레이아웃 수정사항 미반영 해결

---
+++ src/pages/KakaoLogin/KakaoLogin.style.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const LoginContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #252525;
+`;
+
+export const SplashImg = styled.img`
+  width: 100%;
+  padding: 100px 30px 0 30px;
+`;
+
+export const KakaoLoginBtn = styled.img`
+  width: 100%;
+  padding: 0 50px;
+`;
+
+export const LookAroundBtn = styled.div`
+  padding: 20px;
+  font-size: 12px;
+  color: white;
+  text-align: center;
+`;
